713119a56c8e8d60099d31467646879f3757a9ce
Longhao.Chen <Longhao.Chen@outlook.com>
2021-02-14 23:15:35 +0800

    test:Modify the test case.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,15 @@ using Images
 import BM3D
 include("PSNR.jl")
 
-l1 = load("data/Lena512.png")
-l2 = load("data/Lena512_noi_s100.png")
+Lena = load(download("http://www.cs.tut.fi/~foi/GCF-BM3D/images/Lena512.png"))
+σ = [5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 75, 80, 90, 100]
+PSNR_data = [38, 35, 34, 33, 32, 31, 30, 29, 29, 28, 27, 27, 26, 26, 25]
 
 @testset "BM3D" begin
-	l3 = BM3D.bm3d(l2, 0.39)
-	@test PSNR(Float64.(l1), Float64.(l3)) > 23.4
+	for i = 1:length(σ)
+		@testset "σ=$(σ[i])" begin
+			img = load(download("http://www.cs.tut.fi/~foi/GCF-BM3D/images/Lena512_noi_s" * string(σ[i]) * ".png"))
+			@test PSNR(Float64.(Lena), Float64.(BM3D.bm3d(img, σ[i] / 255))) >= PSNR_data[i]
+		end
+	end
 end
