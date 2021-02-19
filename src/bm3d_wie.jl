6a3b61fe52136171ff4276460d45647a33d94cd5
Longhao.Chen <Longhao.Chen@outlook.com>
2021-02-19 22:01:41 +0800

    test:Add PSNR color image support.

--- src/bm3d_wie.jl
+++ src/bm3d_wie.jl
@@ -84,6 +84,6 @@ function wie_3D_filtering!(Wout::Array{<:AbstractFloat, 3},
 			patchSize::Array{Int}, sigma::AbstractFloat)
 	@views @inbounds Threads.@threads for i = 1:size(img, 3)
 		wie_3D_filtering!(Wout[:, :, i], imgOut[:, :, i], G3D[:, :, :, i], G3Dbasic[:, :, :, i], img[:, :, i], imgBasic[:, :, i],
-			matchTable, WC[:, :, :, i], Ilist, Jlist, patchSize, sigma)
+			matchTable, vaild_match, WC[:, :, :, i], Ilist, Jlist, patchSize, sigma)
 	end
 end
