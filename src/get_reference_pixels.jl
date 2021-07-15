fdfad31caa850b469d951fc4eed3076689965bb1
Longhao.Chen <Longhao.Chen@outlook.com>
2021-07-16 00:41:47 +0800

    Complete the improvement of get_reference_pixels.

--- src/get_reference_pixels.jl
+++ src/get_reference_pixels.jl
@@ -1,34 +1,42 @@
 """
 	get_reference_pixels(imgSize::Vector{Int64},
-					patchSize::Vector{Int64},
-					stepSize::Vector{Int64},
-					nBorder::Vector{Int64})
+					patchSize::CartesianIndex{2},
+					searchStride::CartesianIndex{2})
 
 Get locations of reference pixels, the upper-left corner of each patch.
 
 imgSize - size of input image, including border
 patchSize - size of patch in pixels
-stepSize - step in x and y between reference pixels
-nBorder - number of border pixels on each side in x and y
+searchStride - step in x and y between reference pixels
 """
 function get_reference_pixels(imgSize::Vector{Int64},
-				patchSize::Vector{Int64},
-				stepSize::Vector{Int64},
-				nBorder::Vector{Int64})
+				patchSize::CartesianIndex{2},
+				searchStride::CartesianIndex{2})
 
-	ph = imgSize[1] - 2*nBorder[1] - patchSize[1] + 1
-	pw = imgSize[2] - 2*nBorder[2] - patchSize[2] + 1
+	imgH, imgW = imgSize
 
-	I = [nBorder[1] .+ 1:stepSize[1]:ph...]
-	J = [nBorder[2] .+ 1:stepSize[2]:pw...]
-
-	# Make sure there is a patch touching the lower and right borders
-	if maximum(I) < nBorder[1] + ph
-		I = [I; nBorder[1] + ph]
-	end
-	if maximum(J) < nBorder[2] + pw
-		J = [J; nBorder[2] + pw]
+	if (imgH - patchSize[1]) % searchStride[1] != 0
+		tmp_H = 1:searchStride[1]:(imgH - patchSize[1] + 1)
+		H = Vector{UInt}(undef, length(tmp_H) + 1)
+		H[1:end - 1] .= tmp_H
+		H[end] = imgH - patchSize[1] + 1
+	else
+		H = 1:searchStride[1]:(imgH - patchSize[1] + 1)
 	end
 
-	return (I,J)
+	if (imgW - patchSize[2] ) % searchStride[2] != 0
+		tmp_W = 1:searchStride[2]:(imgW - patchSize[2] + 1)
+		W = Vector{UInt}(undef, length(tmp_W) + 1)
+		W[1:end - 1] .= tmp_W
+		W[end] = imgW - patchSize[2] + 1
+	else
+		W = 1:searchStride[2]:(imgW - patchSize[2] + 1)
+	end
+
+	refIndex = Array{CartesianIndex{2}}(undef, length(H), length(W))
+
+	for j in 1:length(W), i in 1:length(H)
+		refIndex[i, j] = CartesianIndex(H[i], W[j])
+	end
+	return refIndex
 end
