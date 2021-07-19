dd78bb8e906e1014719d638009855ecff73843e8
Longhao.Chen <Longhao.Chen@outlook.com>
2021-07-20 00:11:17 +0800

    Complete the improvement of src/groups.jl

--- src/groups.jl
+++ src/groups.jl
@@ -1,144 +1,181 @@
 """
-	form_group!(G3D::Array{Float64, 3},
-					img::Matrix{Float64},
-					matchTable::Array{Float64, 4},
-					Ilist::Vector{Int64},
-					Jlist::Vector{Int64},
-					patchSize::Vector{Int64},
-					position::Tuple{Int, Int})
+	form_group!(G3D::AbstractArray{Float64, 3},
+			img::AbstractArray{Float64, 2},
+			matchTable::Array{CartesianIndex{2}, 3},
+			refIndex::Array{CartesianIndex{2},2},
+			patchSize::CartesianIndex{2},
+			position::CartesianIndex{2},
+			transform_1D!::Function,
+			transform_2D!::Function)
 
 Forward BM3D groupings
 """
-function form_group!(G3D::AbstractArray{Float64, 3},
-			img::AbstractArray{Float64, 2},
-			matchTable::Array{Float64, 4},
-			Ilist::Vector{Int64},
-			Jlist::Vector{Int64},
-			patchSize::Vector{Int64},
-			position::Tuple{Int, Int})
+function form_group!(
+	G3D::AbstractArray{Float64,3},
+	img::AbstractArray{Float64,2},
+	matchTable::Array{CartesianIndex{2},3},
+	refIndex::Array{CartesianIndex{2},2},
+	patchSize::CartesianIndex{2},
+	position::CartesianIndex{2},
+	transform_1D!::Function,
+	transform_2D!::Function,
+)
 
 	# Form table of 3D groups
-	image_to_group!(img, G3D, matchTable, Ilist, Jlist, patchSize, position)
+	image_to_group!(img, G3D, matchTable, refIndex, patchSize, position)
 
-	# Apply 3D DCT on groups.
-	FFTW.dct!(G3D)
+	# Apply 2D transform
+	@inbounds @views Threads.@threads for k in 1:size(G3D, 1)
+		transform_2D!(G3D[k, :, :])
+	end
 
+	# Apply 1D transform
+	@inbounds @views Threads.@threads for j in 1:patchSize[2]
+		for i in 1:patchSize[1]
+			transform_1D!(G3D[:, i, j])
+		end
+	end
 end
 
 """
 	invert_group!(img::AbstractArray{Float64, 2},
 				G3D::AbstractArray{Float64, 3},
-				matchTable::Array{Float64, 4},
-				Ilist::Vector{Int64},
-				Jlist::Vector{Int64},
-				patchSize::Vector{Int64},
-				position::Tuple{Int, Int})
+				matchTable::Array{CartesianIndex{2},3},
+				refIndex::Array{CartesianIndex{2},2},
+				patchSize::CartesianIndex{2},
+				position::CartesianIndex{2},
+				itransform_1D!::Function,
+				itransform_2D!::Function)
 
 Inverse BM3D groupings
 """
-function invert_group!(img::AbstractArray{Float64, 2},
-				G3D::AbstractArray{Float64, 3},
-				matchTable::Array{Float64, 4},
-				Ilist::Vector{Int64},
-				Jlist::Vector{Int64},
-				patchSize::Vector{Int64},
-				position::Tuple{Int, Int})
+function invert_group!(
+	img::AbstractArray{Float64,2},
+	G3D::AbstractArray{Float64,3},
+	matchTable::Array{CartesianIndex{2},3},
+	refIndex::Array{CartesianIndex{2},2},
+	patchSize::CartesianIndex{2},
+	position::CartesianIndex{2},
+	itransform_1D!::Function,
+	itransform_2D!::Function,
+)
 
-	(t, Nmatch) = size(matchTable)
+	# Apply inverse 1D transform
+	@inbounds @views Threads.@threads for j in 1:patchSize[2]
+		for i in 1:patchSize[1]
+			itransform_1D!(G3D[:, i, j])
+		end
+	end
 
-	# Apply inverse 3D DCT on groups
-	FFTW.idct!(G3D)
+	# Apply inverse 2D transform
+	@inbounds @views Threads.@threads for k in 1:size(G3D, 1)
+		itransform_2D!(G3D[k, :, :])
+	end
 
-	group_to_image!(img, G3D, matchTable, Ilist, Jlist, patchSize, position)
+	group_to_image!(img, G3D, matchTable, refIndex, patchSize, position)
 
 end
 
 """
 	group_to_image!(img::AbstractArray{Float64, 2},
-				G3D::AbstractArray{Float64, 3},
-				matchTable::Array{Float64, 4},
-				Ilist::Vector{Int64},
-				Jlist::Vector{Int64},
-				patchSize::Vector{Int64},
-				position::Tuple{Int, Int})
+			G3D::AbstractArray{Float64, 3},
+			matchTable::Array{CartesianIndex{2},3},
+			refIndex::Array{CartesianIndex{2},2},
+			patchSize::CartesianIndex{2},
+			position::CartesianIndex{2})
 
 group to image
 """
-function group_to_image!(img::AbstractArray{Float64, 2},
-				G3D::AbstractArray{Float64, 3},
-				matchTable::Array{Float64, 4},
-				Ilist::Vector{Int64},
-				Jlist::Vector{Int64},
-				patchSize::Vector{Int64},
-				position::Tuple{Int, Int})
+function group_to_image!(
+	img::AbstractArray{Float64,2},
+	G3D::AbstractArray{Float64,3},
+	matchTable::Array{CartesianIndex{2},3},
+	refIndex::Array{CartesianIndex{2},2},
+	patchSize::CartesianIndex{2},
+	position::CartesianIndex{2},
+)
 
-	Nmatch = size(matchTable,2)
-	(i1, j1) = position
+	Nmatch = size(matchTable, 3)
 
-	@strided img[Ilist[i1]:(patchSize[1] - 1 + Ilist[i1]), Jlist[j1]:(patchSize[2] - 1 + Jlist[j1])] .+= G3D[1, 1:patchSize[1], 1:patchSize[2]]
+	@views img[refIndex[position]:(patchSize - CartesianIndex(
+		1,
+		1,
+	) + refIndex[position])] .+= G3D[1, 1:patchSize[1], 1:patchSize[2]]
 
-	@inbounds @views for k = 1:Nmatch
-		i2 = i1 + Int(matchTable[1, k, i1, j1])
-		j2 = j1 + Int(matchTable[2, k, i1, j1])
-		img[Ilist[i2]:(patchSize[1] - 1 + Ilist[i2]), Jlist[j2]:(patchSize[2] - 1 + Jlist[j2])] .+= G3D[k + 1, 1:patchSize[1], 1:patchSize[2]]
+	@inbounds @views for k in 1:Nmatch
+		position2 = position + matchTable[position, k]
+		img[refIndex[position2]:(patchSize - CartesianIndex(
+			1,
+			1,
+		) + refIndex[position2])] .+= G3D[k + 1, 1:patchSize[1], 1:patchSize[2]]
 	end
 end
 
 """
 	group_to_image!(img::AbstractArray{Float64, 2},
-				W::Float64,
-				matchTable::Array{Float64, 4},
-				Ilist::Vector{Int64},
-				Jlist::Vector{Int64},
-				patchSize::Vector{Int64},
-				position::Tuple{Int, Int})
+			W::Float64,
+			matchTable::Array{CartesianIndex{2},3},
+			refIndex::Array{CartesianIndex{2},2},
+			patchSize::CartesianIndex{2},
+			position::CartesianIndex{2})
 """
-function group_to_image!(img::AbstractArray{Float64, 2},
-				W::Float64,
-				matchTable::Array{Float64, 4},
-				Ilist::Vector{Int64},
-				Jlist::Vector{Int64},
-				patchSize::Vector{Int64},
-				position::Tuple{Int, Int})
+function group_to_image!(
+	img::AbstractArray{Float64,2},
+	W::Float64,
+	matchTable::Array{CartesianIndex{2},3},
+	refIndex::Array{CartesianIndex{2},2},
+	patchSize::CartesianIndex{2},
+	position::CartesianIndex{2},
+)
 
-	Nmatch = size(matchTable,2)
-	(i1, j1) = position
+	nMatch = size(matchTable, 3)
 
-	@strided img[Ilist[i1]:(patchSize[1] - 1 + Ilist[i1]), Jlist[j1]:(patchSize[2] - 1 + Jlist[j1])] .+= W
+	@views img[refIndex[position]:(patchSize - CartesianIndex(
+		1,
+		1,
+	) + refIndex[position])] .+= W
 
-	@inbounds @views for k = 1:Nmatch
-		i2 = i1 + Int(matchTable[1, k, i1, j1])
-		j2 = j1 + Int(matchTable[2, k, i1, j1])
-		img[Ilist[i2]:(patchSize[1] - 1 + Ilist[i2]), Jlist[j2]:(patchSize[2] - 1 + Jlist[j2])] .+= W
+	@inbounds @views for k in 1:nMatch
+		position2 = position + matchTable[position, k]
+		img[refIndex[position2]:(patchSize - CartesianIndex(
+			1,
+			1,
+		) + refIndex[position2])] .+= W
 	end
 end
 
 """
 	image_to_group!(img::AbstractArray{Float64, 2},
-					G3D::AbstractArray{Float64, 3},
-					matchTable::Array{Float64, 4},
-					Ilist::Vector{Int64},
-					Jlist::Vector{Int64},
-					patchSize::Vector{Int64},
-					position::Tuple{Int, Int})
+			G3D::AbstractArray{Float64, 3},
+			matchTable::Array{CartesianIndex{2},3},
+			refIndex::Array{CartesianIndex{2},2},
+			patchSize::CartesianIndex{2},
+			position::CartesianIndex{2})
 
 """
-function image_to_group!(img::AbstractArray{Float64, 2},
-				G3D::AbstractArray{Float64, 3},
-				matchTable::Array{Float64, 4},
-				Ilist::Vector{Int64},
-				Jlist::Vector{Int64},
-				patchSize::Vector{Int64},
-				position::Tuple{Int, Int})
+function image_to_group!(
+	img::AbstractArray{Float64,2},
+	G3D::AbstractArray{Float64,3},
+	matchTable::Array{CartesianIndex{2},3},
+	refIndex::Array{CartesianIndex{2},2},
+	patchSize::CartesianIndex{2},
+	position::CartesianIndex{2},
+)
 
-	Nmatch = size(matchTable,2)
-	i1, j1 = position
+	nMatch = size(matchTable, 3)
 
-	G3D[1, 1:patchSize[1], 1:patchSize[2]] .= img[Ilist[i1]:(patchSize[1] - 1 + Ilist[i1]), Jlist[j1]:(patchSize[2] - 1 + Jlist[j1])]
+	@views G3D[1, CartesianIndex(1, 1):patchSize] .=
+		img[refIndex[position]:(patchSize - CartesianIndex(
+			1,
+			1,
+		) + refIndex[position])]
 
-	@views @inbounds for k = 1:Nmatch
-		i2 = i1 + Int(matchTable[1, k, i1, j1])
-		j2 = j1 + Int(matchTable[2, k, i1, j1])
-		G3D[k + 1, 1:patchSize[1], 1:patchSize[2]] .= img[Ilist[i2]:(patchSize[1] - 1 + Ilist[i2]), Jlist[j2]:(patchSize[2] - 1 + Jlist[j2])]
+	@views @inbounds for k in 1:nMatch
+		position2 = position + matchTable[position, k]
+		G3D[k + 1, CartesianIndex(1, 1) + patchSize] .=
+			img[refIndex[position2]:(patchSize - CartesianIndex(
+				1,
+				1,
+			) + refIndex[position2])]
 	end
 end
