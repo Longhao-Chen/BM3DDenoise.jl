3702438fd4d0e332980677ccea9432aae12eff63
Longhao.Chen <Longhao.Chen@outlook.com>
2021-07-20 11:20:33 +0800

    adjust test: test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,31 +9,52 @@ PSNR_data = [38, 35, 33, 32, 31, 30, 29, 28, 27, 26, 25, 25, 25, 24, 23]
 # download file
 if !isfile(joinpath(tempdir(), "Lena512.png"))
 	println("Downloading Lena512.png")
-	Downloads.download("http://www.cs.tut.fi/~foi/GCF-BM3D/images/Lena512.png", joinpath(tempdir(), "Lena512.png"))
+	Downloads.download(
+		"http://www.cs.tut.fi/~foi/GCF-BM3D/images/Lena512.png",
+		joinpath(tempdir(), "Lena512.png"),
+	)
 end
 if !isfile(joinpath(tempdir(), "image_Lena512rgb_noi_s100.png"))
 	println("Downloading image_Lena512rgb_noi_s100.png")
-	Downloads.download("http://www.cs.tut.fi/~foi/GCF-BM3D/images/image_Lena512rgb_noi_s100.png", joinpath(tempdir(), "image_Lena512rgb_noi_s100.png"))
+	Downloads.download(
+		"http://www.cs.tut.fi/~foi/GCF-BM3D/images/image_Lena512rgb_noi_s100.png",
+		joinpath(tempdir(), "image_Lena512rgb_noi_s100.png"),
+	)
 end
 if !isfile(joinpath(tempdir(), "image_Lena512rgb.png"))
 	println("Downloading image_Lena512rgb.png")
-	Downloads.download("http://www.cs.tut.fi/~foi/GCF-BM3D/images/image_Lena512rgb.png", joinpath(tempdir(), "image_Lena512rgb.png"))
+	Downloads.download(
+		"http://www.cs.tut.fi/~foi/GCF-BM3D/images/image_Lena512rgb.png",
+		joinpath(tempdir(), "image_Lena512rgb.png"),
+	)
 end
-for i in σ
+Threads.@threads for i in σ
 	if !isfile(joinpath(tempdir(), "Lena512_noi_s" * string(i) * ".png"))
 		println("Downloading Lena512_noi_s" * string(i) * ".png")
-		Downloads.download("http://www.cs.tut.fi/~foi/GCF-BM3D/images/Lena512_noi_s" * string(i) * ".png",
-			joinpath(tempdir(), "Lena512_noi_s" * string(i) * ".png"))
+		Downloads.download(
+			"http://www.cs.tut.fi/~foi/GCF-BM3D/images/Lena512_noi_s" *
+			string(i) *
+			".png",
+			joinpath(tempdir(), "Lena512_noi_s" * string(i) * ".png"),
+		)
 	end
 end
 
 
 Lena = load(joinpath(tempdir(), "Lena512.png"))
 @testset "BM3D_gray" begin
-	for i = 1:length(σ)
+	for i in 1:length(σ)
 		@testset "σ=$(σ[i])" begin
-			img = load(joinpath(tempdir(), "Lena512_noi_s" * string(σ[i]) * ".png"))
-			bm3d_psnr = assess_psnr(Float64.(Lena), Float64.(BM3D.bm3d(img, σ[i] / 255)))
+			img = load(
+				joinpath(
+					tempdir(),
+					"Lena512_noi_s" * string(σ[i]) * ".png",
+				),
+			)
+			bm3d_psnr = assess_psnr(
+				Lena,
+				BM3D.bm3d(img, σ[i] / 255),
+			)
 			println("σ=$(σ[i]), $(bm3d_psnr)")
 			@test bm3d_psnr >= PSNR_data[i]
 		end
@@ -43,7 +64,7 @@ end
 Lena = load(joinpath(tempdir(), "image_Lena512rgb.png"))
 @testset "BM3D_color" begin
 	img = load(joinpath(tempdir(), "image_Lena512rgb_noi_s100.png"))
-	bm3d_psnr = assess_psnr(Lena, BM3D.bm3d(img, 100/255))
+	bm3d_psnr = assess_psnr(Lena, BM3D.bm3d(img, 100 / 255))
 	println("σ=100(rgb), $(bm3d_psnr)")
 	@test bm3d_psnr >= 22
 end
