d21cb6ad7bf72b22b6c4194da3610331f2f55566
Longhao.Chen <Longhao.Chen@outlook.com>
2021-07-20 10:01:52 +0800

    Complete the improvement of src/bm3d_wie.jl

--- src/bm3d_wie.jl
+++ src/bm3d_wie.jl
@@ -2,34 +2,60 @@
 	bm3d_wie(img::Array{Float64}, imgBasic::Array{Float64}, sigma::AbstractFloat, config::bm3d_config)
 
 2nd step of BM3D
+
 img: input noisy image
 imgBasic: denoised image from first step of BM3D (hard thresholding)
 sigma: known or assumed standard deviation of noise
+config: bm3d_config
 """
-function bm3d_wie(img::Array{Float64}, imgBasic::Array{Float64}, sigma::AbstractFloat, config::bm3d_config)
+function bm3d_wie(
+	img::Array{Float64},
+	imgBasic::Array{Float64},
+	sigma::AbstractFloat,
+	config::bm3d_config,
+)
 
 	# parameters
 	patchSize = config.wie_patchSize
-	stepSize = config.wie_stepSize
-	nBorder = config.wie_nBorder
+	searchStride = config.wie_searchStride
 	searchWin = config.wie_searchWin
 	nMatch = config.wie_nMatch
-	thresh3D =config.wie_thresh3D
 
 	# block matching step
 	@info "2st get_reference_pixels"
-	(Ilist,Jlist) = get_reference_pixels([size(img,1);size(img,2)],patchSize,stepSize,nBorder)
+	refIndex =
+		get_reference_pixels([size(img, 1); size(img, 2)], patchSize, searchStride)
+	@info "2st get_reference_pixels end"
 	@info "2st match_patches"
-	matchTable = match_patches(imgBasic,Ilist,Jlist,patchSize,searchWin,nMatch)
+	matchTable = match_patches(
+		imgBasic,
+		refIndex,
+		patchSize,
+		searchWin,
+		nMatch,
+		config.wie_distFunction,
+	)
 	@info "2st match_patches end"
 
-	Wout = zeros(Float64, size(img))
-	imgOut = zeros(Float64, size(img))
+	Wout = similar(img)
+	imgOut = similar(img)
 
 	# 3D filtering
 	@info "2st 3D filtering"
-	wie_3D_filtering!(Wout, imgOut, img, imgBasic, matchTable, Ilist, Jlist, patchSize, searchWin, nMatch, sigma)
-	
+	wie_3D_filtering!(
+		Wout,
+		imgOut,
+		img,
+		imgBasic,
+		matchTable,
+		refIndex,
+		patchSize,
+		nMatch,
+		sigma,
+		config,
+	)
+	@info "2st 3D filtering end"
+
 	return imgOut ./ Wout
 
 end
@@ -37,54 +63,108 @@ end
 """
 3D filtering
 """
-function wie_3D_filtering!(Wout::AbstractArray{<:AbstractFloat, 2},
-			imgOut::AbstractArray{<:AbstractFloat, 2},
-			img::AbstractArray{<:AbstractFloat, 2},
-			imgBasic::AbstractArray{<:AbstractFloat, 2},
-			matchTable::Array{<:AbstractFloat},
-			Ilist::Array{Int}, Jlist::Array{Int},
-			patchSize::Array{Int}, searchWin::Array{Int},
-			nMatch::Int, sigma::AbstractFloat)
+function wie_3D_filtering!(
+	Wout::AbstractArray{<:AbstractFloat,2},
+	imgOut::AbstractArray{<:AbstractFloat,2},
+	img::AbstractArray{<:AbstractFloat,2},
+	imgBasic::AbstractArray{<:AbstractFloat,2},
+	matchTable::Array{CartesianIndex{2},3},
+	refIndex::Array{CartesianIndex{2},2},
+	patchSize::CartesianIndex{2},
+	nMatch::Int,
+	sigma::AbstractFloat,
+	config::bm3d_config,
+)
 	# Each reference block is processed to reduce memory usage
-	I_end = length(Ilist)
-	J_end = length(Jlist)
-	# Preventing conflicts in parallel computing
-	@views @inbounds for offset in 0:2searchWin[2] - 1
-		Threads.@threads for J = 1 + offset:2searchWin[2]:J_end
-			G3D = zeros(Float64, nMatch+1, patchSize[1], patchSize[2])
-			G3Dbasic = similar(G3D)
-			WC = similar(G3Dbasic)
-			for I = 1:I_end
-				# Compute 3D group spectrum
-				form_group!(G3D, img, matchTable, Ilist, Jlist, patchSize, (I, J))
-				form_group!(G3Dbasic, imgBasic, matchTable, Ilist, Jlist, patchSize, (I, J))
+	I_end, J_end = size(refIndex)
 
-				# Wiener filtering of 3D groups, using basic estimate as target spectrum
-				WC .= G3Dbasic.^2 ./ (G3Dbasic.^2 .+ sigma^2)
-				G3D .*= WC
+	@views @inbounds Threads.@threads for J in 1:J_end
+		# Preventing conflicts in parallel computing
+		G3D = zeros(Float64, nMatch + 1, patchSize[1], patchSize[2])
+		G3Dbasic = similar(G3D)
+		WC = similar(G3Dbasic)
+		for I in 1:I_end
+			# Compute 3D group spectrum
+			form_group!(
+				G3D,
+				img,
+				matchTable,
+				refIndex,
+				patchSize,
+				CartesianIndex(I, J),
+				config.wie_transform_1D!,
+				config.wie_transform_2D!,
+			)
+			form_group!(
+				G3Dbasic,
+				imgBasic,
+				matchTable,
+				refIndex,
+				patchSize,
+				CartesianIndex(I, J),
+				config.wie_transform_1D!,
+				config.wie_transform_2D!,
+			)
 
-				# Weight
-				T = norm(WC, 1)
-				W = T > 0 ? 1.0/(T *sigma^2) : 1.0
+			# Wiener filtering of 3D groups, using basic estimate as target spectrum
+			WC .= G3Dbasic .^ 2 ./ (G3Dbasic .^ 2 .+ sigma^2)
+			G3D .*= WC
+
+			# Weight
+			T = norm(WC, 1)
+			if T > zero(T)
+				W = 1.0 / (T * sigma^2)
 				G3D .*= W
-
-				invert_group!(imgOut, G3D, matchTable, Ilist, Jlist, patchSize, (I, J))
-				group_to_image!(Wout, W, matchTable, Ilist, Jlist, patchSize, (I, J))
+			else
+				W = 1.0
 			end
+
+			invert_group!(
+				imgOut,
+				G3D,
+				matchTable,
+				refIndex,
+				patchSize,
+				CartesianIndex(I, J),
+				config.wie_itransform_1D!,
+				config.wie_itransform_2D!,
+			)
+			group_to_image!(
+				Wout,
+				W,
+				matchTable,
+				refIndex,
+				patchSize,
+				CartesianIndex(I, J),
+			)
 		end
 	end
 end
 
-function wie_3D_filtering!(Wout::Array{<:AbstractFloat, 3},
-			imgOut::Array{<:AbstractFloat, 3},
-			img::Array{<:AbstractFloat, 3},
-			imgBasic::Array{<:AbstractFloat, 3},
-			matchTable::Array{<:AbstractFloat},
-			Ilist::Array{Int}, Jlist::Array{Int},
-			patchSize::Array{Int}, searchWin::Array{Int},
-			nMatch::Int, sigma::AbstractFloat)
-	@views @inbounds Threads.@threads for i = 1:size(img, 3)
-		wie_3D_filtering!(Wout[:, :, i], imgOut[:, :, i], img[:, :, i], imgBasic[:, :, i],
-			matchTable, Ilist, Jlist, patchSize, searchWin, nMatch, sigma)
+function wie_3D_filtering!(
+	Wout::Array{<:AbstractFloat,3},
+	imgOut::Array{<:AbstractFloat,3},
+	img::Array{<:AbstractFloat,3},
+	imgBasic::Array{<:AbstractFloat,3},
+	matchTable::Array{CartesianIndex{2},3},
+	refIndex::Array{CartesianIndex{2},2},
+	patchSize::CartesianIndex{2},
+	nMatch::Int,
+	sigma::AbstractFloat,
+	config::bm3d_config,
+)
+	@views @inbounds for i in 1:size(img, 3)
+		wie_3D_filtering!(
+			Wout[:, :, i],
+			imgOut[:, :, i],
+			img[:, :, i],
+			imgBasic[:, :, i],
+			matchTable,
+			refIndex,
+			patchSize,
+			nMatch,
+			sigma,
+			config,
+		)
 	end
 end
